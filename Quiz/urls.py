b66821097d1df9f6eeb0235fbf4311e781916fef
Nasir Noor <nasirnoor732@gmail.com>
2023-08-03 22:57:55 +0500

    athentication done

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name= "Quiz"
 urlpatterns = [
     path('', views.signupPage , name='signup'),
+    path('login/', views.loginPage , name='login'),
+    path('home/', views.HomePage , name='home'),
+    path('logout/', views.logoutpage , name='logout'),
 ]
+
 
 
--- Quiz/views.py
+++ Quiz/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User 
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
 def HomePage(request):
-    pass
+    user = request.user
+    print(user.id)
+    return render(request,'Quiz/home.html')
 
 def loginPage(request):
-    pass
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=uname, password=pass1)
+
+        if user is not None:
+            login(request,user)
+            return redirect("Quiz:home")
+        else:
+            return HttpResponse("username or password is incorrect")
+    return render(request,'Quiz/login.html')
 
 def signupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse ("your passwoerd and confirm password is not same!!")
+        else:
+
+           my_user=User.objects.create_user(uname,email,pass1)
+           my_user.save()
+           return redirect("Quiz:login")
+    
+
     return render(request,'Quiz/signup.html')
+
+def logoutpage(request):
+    logout(request)
+    return redirect('Quiz:login')
